51f5d9daa31f6dc10390dc3c9a2feb283b90a70f
Liam Cunningham <tuo20284@temple.edu>
2024-02-08 12:01:32 -0500

    Created Drop Down View Function

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,13 +1,14 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
 class TextSizeAdapter : BaseAdapter() {
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
     }
 
     override fun getItem(position: Int): Any {
@@ -20,6 +21,22 @@ class TextSizeAdapter : BaseAdapter() {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         TODO("Not yet implemented")
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View{
+        val textView : TextView
+
+        if (convertView != null) {
+            textView = convertView as TextView
+        } else {
+            textView = TextView(context)
+            textView.setPadding(5, 10, 0, 10)
+        }
+
+        textView.text = fontSizes[position].toString()
+        textView.textSize = fontSizes[position].toFloat()
+
+        return textView
     }
 
 }
